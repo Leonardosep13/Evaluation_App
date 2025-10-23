ba197168a5685334f61bf3f46a62d03da30ccc4a
jaime Leonardo Ríos Magaña <jaime.10leonardo@gmail.com>
2025-10-23 12:48:30 -0600

    Flujo de datos solucionado, ahora los datos se envian desde la page al componente

--- class-rank-front/src/pages/Teachers.jsx
+++ class-rank-front/src/pages/Teachers.jsx
@@ -1,34 +1,68 @@
-import React, { useState } from 'react';
-import { Button, Row, Col, Alert } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Button, Row, Col} from 'react-bootstrap';
 import { PageHeader, BasicModal } from '../components/common';
-import { TeacherCard} from '../components/TeacherCard';
-import { CreateTeacherForm } from '../components/TeachersForms/CreateTeacherForm';
+import { TeacherCard} from '../components/TeachersAdmin/TeacherCard';
+import { CreateTeacherForm } from '../components/TeachersAdmin/TeachersForms/CreateTeacherForm';
 import { useUser } from '../hooks/useUser';
+import { SuccessAlert, ErrorAlert } from '../components/Alerts/GenericAlert';
 
 export function Profesores() {
   const [showModal, setShowModal] = useState(false);
-  const [success, setSuccess] = useState(false);
-  const { createUser, loading, error } = useUser();
+  const { createUser, deleteUser, loading, error, users, getUsers } = useUser();
+
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   const handleShowModal = () => setShowModal(true);
-  const handleCloseModal = () => {
-    setShowModal(false);
-    setSuccess(false);
-  };
+  const handleCloseModal = () => { setShowModal(false);};
 
   const handleCreateTeacher = async (formData) => {
     try {
       await createUser(formData);
-      setSuccess(true);
-      getUsers();
+      await getUsers();
+      SuccessAlert(
+        'Profesor creado',
+        'El profesor ha sido creado exitosamente.'
+      );
       setTimeout(() => {
         handleCloseModal();
       }, 2000);
     } catch (error) {
-      console.error('Error creating teacher:', error);
+      ErrorAlert(
+        'Error',
+        'Error al crear el profesor. Por favor, inténtalo de nuevo.'
+      );
     }
   };
 
+  const handleDeleteTeacher = async (userId) => {
+    try {
+      await deleteUser(userId);
+      await getUsers();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const handleUpdateTeacher = async () => {
+  try{
+    await getUsers();
+    SuccessAlert(
+      'Profesor actualizado',
+      'El profesor ha sido actualizado exitosamente.'
+    );
+    setTimeout(() => {
+      handleCloseModal();
+    }, 2000);
+  } catch (error) {
+    ErrorAlert(
+      'Error',
+      'Error al actualizar el profesor. Por favor, inténtalo de nuevo.'
+    );
+  };
+  }
+  
   return (
     <div>
       <PageHeader
@@ -48,15 +82,13 @@ export function Profesores() {
             </Button>
           </Col>
         </Row>
-
-        {success && (
-          <Alert variant="success" className="mb-3">
-            <i className="bi bi-check-circle me-2"></i>
-            ¡Profesor registrado exitosamente!
-          </Alert>
-        )}
-
-        <TeacherCard/>
+        <TeacherCard
+          users={users}
+          loading={loading}
+          error={error}
+          onDeleteUser={handleDeleteTeacher}
+          onUpdateUser={handleUpdateTeacher}
+        />
       </PageHeader>
 
       <BasicModal
