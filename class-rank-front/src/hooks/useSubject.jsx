e4bdb7677ed44b299b7d67f8852689577c004b68
jaime Leonardo Ríos Magaña <jaime.10leonardo@gmail.com>
2025-10-24 12:47:23 -0600

    CustomHook para el manejo de materias

--- class-rank-front/src/hooks/useSubject.jsx
+++ class-rank-front/src/hooks/useSubject.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useAuth } from "./useAuth";
+import { getSubjectsApi, createSubjectApi, deleteSubjectApi} from "../api/subject";
 
 export function useSubject() {
     const [loading, setLoading] = useState(false);
@@ -20,11 +21,45 @@ export function useSubject() {
         }
     };
 
+    const createSubject = async (formValue) => {
+        try {
+            setLoading(true);
+            setError(null);
+            const response = await createSubjectApi(auth.token.access, formValue);
+            setLoading(false);
+            return response;
+        }
+        catch (error) { 
+            setError(error);
+            setLoading(false);
+            throw error;
+        }
+    };
+
+    const deleteSubject = async (subjectId) => {
+        try{
+            setLoading(true);
+            setError(null);
+            const response = await deleteSubjectApi(auth.token.access, subjectId);
+            setLoading(false);
+            return response;
+        }
+        catch (error){
+            setError(error);
+            setLoading(false);
+            throw error;
+        }
+    };
+
+
+
     return {
         loading,
         error,
         subjects,
         getSubjects,
+        createSubject,
+        deleteSubject,
     };
 
 }
