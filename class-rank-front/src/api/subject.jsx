16074d4951aec1acb4c712335f9b55fbc174be57
jaime Leonardo Ríos Magaña <jaime.10leonardo@gmail.com>
2025-10-24 12:46:31 -0600

    CallApi con mas manejo de errores para funcion de borrar

--- class-rank-front/src/api/subject.jsx
+++ class-rank-front/src/api/subject.jsx
@@ -2,7 +2,7 @@ import { BASE_API } from "../utils/constants";
 
 export async function getSubjectsApi(token) {
     try {
-        const url = `${BASE_API}/subjects/`;
+        const url = `${BASE_API}/academics/subjects/`;
         const params = {
             method: "GET",
             headers: {
@@ -23,4 +23,49 @@ export async function getSubjectsApi(token) {
         throw error;
     }
 }
-    
+
+export async function createSubjectApi(token, formValue) {
+    try{
+        const url = `${BASE_API}/academics/subjects/`;
+        const params = {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(formValue),
+        };
+        const response = await fetch(url, params);
+        if (response.status !== 201){
+            const result = await response.json();
+            throw result;
+        }
+        const result = await response.json();
+        return result;
+    }
+    catch (error){
+        throw error;
+    }
+}
+
+export async function deleteSubjectApi(token, subjectId) {
+    try{
+        const url = `${BASE_API}/academics/subjects/${subjectId}/`;
+        const params = {
+            method: "DELETE",
+            headers: {
+                "content-type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        };
+        const response = await fetch(url, params);
+        if (response.status !== 204){
+            const result = await response.json();
+            throw result;
+        }
+        return;
+    }
+    catch (error){
+        throw error;
+    }
+}
