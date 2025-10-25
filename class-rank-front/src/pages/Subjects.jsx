c938ddfcabe95a958100263a0c302a20c212bff7
jaime Leonardo Ríos Magaña <jaime.10leonardo@gmail.com>
2025-10-25 13:17:19 -0600

    Implementacion de borrar,editar y añadir materia

--- class-rank-front/src/pages/Subjects.jsx
+++ class-rank-front/src/pages/Subjects.jsx
@@ -3,12 +3,64 @@ import { PageHeader, BasicModal } from '../components/common';
 import { Row, Col, Button } from 'react-bootstrap';
 import { useSubject } from '../hooks/useSubject';
 import { SubjectCard } from '../components/SubjectsAdmin/SubjectCard';
+import { CreateSubjectForm } from '../components/SubjectsAdmin/SubjectsForms/CreateSubjectForm/CreateSubjectForm';
+import { ErrorAlert, SuccessAlert } from '../components/Alerts/GenericAlert';
 
 export function Materias() {
   const [showModal, setShowModal] = useState(false);
-  const {subjects, loading, error, getSubjects} = useSubject();
+  const {subjects, loading, error, getSubjects, deleteSubject, createSubject} = useSubject();
   const handleShowModal = () => setShowModal(true);
   const handleCloseModal = () => setShowModal(false);
+
+  const handleCreateSubject = async (formData) => {
+    try {
+      await createSubject(formData);
+      await getSubjects();
+      SuccessAlert(
+        'Materia creada',
+        'La materia ha sido creada exitosamente.'
+      );
+      setTimeout(() => {
+        handleCloseModal();
+      }, 2000);
+    } catch (error) {
+      ErrorAlert(
+        'Error',
+        'Error al crear la materia. Por favor, inténtalo de nuevo.'
+      );
+
+
+      throw error;
+    }
+  };
+
+  const handleDeleteSubject = async (subjectId) => {
+    try {
+      await deleteSubject(subjectId);
+      await getSubjects();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const handleUpdateSubject = async () => {
+    try{
+      await getSubjects();
+      SuccessAlert(
+        'Materia actualizada',
+        'La materia ha sido actualizada exitosamente.'
+      );
+      setTimeout(() => {
+        handleCloseModal();
+      }, 2000);
+    }
+      catch (error) {
+      ErrorAlert(
+        'Error',
+        'Error al actualizar la materia. Por favor, inténtalo de nuevo.'
+      );
+    }
+  };
 
   useEffect(() => {
     getSubjects();
@@ -38,6 +90,8 @@ export function Materias() {
           subjects={subjects}
           loading={loading}
           error={error}
+          onDeleteSubject={handleDeleteSubject}
+          onUpdateSubject={handleUpdateSubject}
         />
         
     </PageHeader>
@@ -46,10 +100,15 @@ export function Materias() {
         title="Registrar Nueva Materia"
         show={showModal}
         handleClose={handleCloseModal}
-        actionButtonDescription="Registrar"
+        actionButtonDescription="Registrar materia"
+        loading={loading}
         size="lg"
         >
-
+          <CreateSubjectForm 
+          onSubmit={handleCreateSubject}
+          loading={loading}
+          error={error}
+        />
         </BasicModal>
   </div>  
   );
