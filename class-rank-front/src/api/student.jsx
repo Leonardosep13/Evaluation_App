c03fade3a30c4157852c3e0623b928d40fd189ea
jaime Leonardo Ríos Magaña <jaime.10leonardo@gmail.com>
2025-10-27 12:51:42 -0600

    ApiCall para estudiantes. Falta editar y borrar

---
+++ class-rank-front/src/api/student.jsx
@@ -0,0 +1,25 @@
+import { BASE_API } from "../utils/constants";
+
+export async function getStudentsApi(token)
+{
+    try{
+        const url = `${BASE_API}/academics/students/`;
+        const params = {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        };
+        const response = await fetch(url, params);
+        if (response.status !== 200) {
+            const result = await response.json();
+            throw result;
+        }
+        const result = await response.json();
+        return result;
+    }
+    catch (error){
+        throw error;
+    }
+}
