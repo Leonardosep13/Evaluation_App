4cad72802ebd5f55030543ea5440de258ff0f9d5
jaime Leonardo Ríos Magaña <jaime.10leonardo@gmail.com>
2025-10-28 12:52:29 -0600

    Metodos put, delete y create añadidos

--- class-rank-front/src/api/student.jsx
+++ class-rank-front/src/api/student.jsx
@@ -23,3 +23,52 @@ export async function getStudentsApi(token)
         throw error;
     }
 }
+
+export async function createStudentApi(token, formValue)
+{
+    try{
+        const url = `${BASE_API}/academics/students/`;
+        const params = {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(formValue),
+        };
+        const response = await fetch(url, params);
+        if (response.status !== 201){
+            const result = await response.json();
+            throw result;
+    }
+        const result = await response.json();
+        return result;
+    }
+    catch (error){
+        throw error;
+    }
+
+}
+
+export async function deleteStudentApi(token, studentId)
+{
+    try{
+        const url = `${BASE_API}/academics/students/${studentId}/`;
+        const params = {
+            method: "DELETE",
+            headers: {
+                "content-type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        };
+        const response = await fetch(url, params);
+        if (response.status !== 204){
+            const result = await response.json();
+            throw result;
+        }
+        return;
+    }
+    catch (error){
+        throw error;
+    }
+}
