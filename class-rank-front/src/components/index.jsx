5f3923484bb77f1afe0a2ee87fb53259b897ab2f
jaime Leonardo Ríos Magaña <jaime.10leonardo@gmail.com>
2025-10-28 12:53:00 -0600

    indice actualizado

--- class-rank-front/src/components/index.jsx
+++ class-rank-front/src/components/index.jsx
@@ -2,5 +2,5 @@ export * from './LoginForm';
 export * from './Sidebar';
 export * from './TeachersAdmin';
 export * from './common';
-export * from './StudentsTable';
+export * from './StudentsAdmin';
 export * from './Alerts';
